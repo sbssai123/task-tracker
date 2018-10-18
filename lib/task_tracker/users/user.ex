9e6b7420f433d15b40870e4e8703439a858309d0
sbssai123 <sbs.sai@gmail.com>
2018-10-18 18:14:17 -0400

    unique emails

--- lib/task_tracker/users/user.ex
+++ lib/task_tracker/users/user.ex
@@ -1,3 +1,5 @@
+# Use this tutorial to validate inputs when creating a user:
+# https://medium.com/@diamondgfx/testing-validations-in-elixir-and-ecto-677bd8a071a1
 defmodule TaskTracker.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
@@ -16,5 +18,6 @@ defmodule TaskTracker.Users.User do
     user
     |> cast(attrs, [:first_name, :last_name, :email])
     |> validate_required([:first_name, :last_name, :email])
+    |> unique_constraint(:email)
   end
 end
